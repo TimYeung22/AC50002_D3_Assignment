94f2549954f8d24b272d6d63e9bd74a104827c5e
Tim <timyyeung2@gmail.com>
2022-10-29 21:20:48 +0100

    First change

--- scripts/d3_assignment.js
+++ scripts/d3_assignment.js
@@ -1,88 +1,57 @@
-var svg;
-function d3Draw(dataset){
-Width=1000;
-Height=800;
-if ((typeof svg == 'undefined') ){
-   svg= d3.select("body").append("svg")
-   .attr("width",Width)
-   .attr("Height",Height);
-}
-}
-d3.json("countries.geojson",function(d){
-	var uk = d3.select("ISO_A3").property("value", "GBR");
-	console.log(uk);
-	var group = svg.selectAll("geometry").data(d.coordinates).data(d.properties).enter().append("geometry");
-	var projection = d3.geo.mercator().scale(2000);
-	var path = d3.geo.path().projection(projection);
-	var area = group.append("path").attr("d".path).attr("class","area").attr("fill","steelblue");
+var width = 1500;
+	height = 2000;
+var map = document.getElementById("map");
+var addsvg = d3.select(".map").append("svg").attr("width", width).attr("height", height);
+var svg = d3.select("svg");
+var projection = d3.geoMercator().scale(5500).center([-7,60]);
+var pathGenerator = d3.geoPath().projection(projection);
+var ukmap = 'https://raw.githubusercontent.com/deldersveld/topojson/master/countries/united-kingdom/uk-counties.json';
+d3.json(ukmap).then(data =>{ 
+	console.log(data);
+	var cities = topojson.feature(data, data.objects.GBR_adm2);
+	svg.selectAll("path").data(cities.features).enter().append("path").attr("d", pathGenerator);
+	console.log(cities.features); 	 	
 });
-
-/*var circles=svg.selectAll("circle")
-   .data(dataset)
-   .enter()
-   .append("circle");
-
-circles.attr("cx", function(d){
-    return d.x;
-} )
-.attr("cy",function(d){
-    return d.y;
-} )
-.attr("r", function(d){
-    return d.r;
-} );  
-   
-
-
-function d3Update(dataset){
-
-	var circles=svg.selectAll("circle")
-	.data(dataset)
-	.transition()
-	.duration(500)
-	.ease("bounce");
-
-	circles.attr("cx", function(d){
-	    return d.x;
-	} )
-	.attr("cy",function(d){
-	    return d.y;
-	} )
-	.attr("r", function(d){
-	    return d.r;
-	} );  
-	   
-	}*/
-
-function loadData(){
-
-
-	d3.select("p")
-	.on("click",function(){
-		
-		updateData();
+function ChangeSlider(n){
+	var slidervalue =  document.getElementById("SizeSlider").value;
+	document.getElementById("SizeSlider").value = parseInt(slidervalue) + n;
+	UpdateValue();
+}
+function showTooltip(x,y,text){
+	console.log(text);
+	let tooltip = document.getElementById("Tooltip");
+	tooltip.innerHTML = "<text>"+text+"</text>";
+	tooltip.style.display = "block";
+	tooltip.style.left = x + "px";
+	tooltip.style.top = y + 140 + "px";
+}
+function hideToolTip(){
+	let tooltip = document.getElementById("Tooltip");
+	tooltip.style.display = "none";
+}
+function UpdateValue(){
+	var slidervalue =  document.getElementById("SizeSlider").value;
+	var sliderDisplay = document.getElementById("slidernum");
+	sliderDisplay.innerHTML = slidervalue;
+}
+function CreateTowns(){
+	var slidervalue =  document.getElementById("SizeSlider").value;
+	var datalink = 'http://34.78.46.186/Circles/Towns/'+slidervalue;
+	svg.selectAll("circle").transition().duration(300).style("opacity", 0);
+	svg.selectAll("circle").remove();
+	d3.json(datalink).then(function(d){
+		svg.selectAll("circle").data(d).enter().append("circle")
+		.attr("cx", function(data){var coords = projection([data.lng,data.lat]); return coords[0];})
+		.attr("cy", function(data){var coords = projection([data.lng,data.lat]); return coords[1];})
+		.attr("r", function(data){var Psize = 1+2*Math.sqrt(data.Population/10000); return Psize;})
+		.attr("onmousemove", function(data){var coords = projection([data.lng,data.lat]);var msg = "showTooltip("+coords[0]+","+coords[1]+","+'"Town Name: '+data.Town+"<br>Population: "+data.Population+'");'; return msg;})
+		.attr("onmouseout", function(data){var msg = "hideToolTip();"; return msg;})
+		.style("opacity", 0)
+		.transition()
+		.duration(300)
+		.style("opacity", 0.7)
 	});
-   d3.json("http://34.78.46.186/Circles/Towns/50",function(error,data){
-   if (error){
-      console.log(error)
-   }else{
-      d3Draw(data);
-      }
-   }
-   );
 }
- 
-function updateData(){
-  
-   d3.json("http://34.78.46.186/Circles/Towns/50",function(error,data){
-   if (error){
-      console.log(error)
-   }else{
-      d3Update(data);
-      }
-   }
-   );
-}
-
-window.onload= loadData;
+window.onload=UpdateValue();
+window.onload=CreateTowns();
 
